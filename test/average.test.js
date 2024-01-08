e2dbf9fc1c2f5bda33c84a3daea8ad2c788440ed
Lucas Charoing <charoing.lucas@gmail.com>
2024-01-08 11:59:26 +0100

    Add multiple tests

--- test/average.test.js
+++ test/average.test.js
@@ -1,5 +1,12 @@
 import {average} from '../src/average';
-test('average', () => {
+test('average of positive numbers', () => {
     expect(average([1, 2, 3, 4, 5])).toBe(3);
 });
 
+test('average of negative numbers', () => {
+    expect(average([-1, -2, -3, -4, -5])).toBe(-3);
+});
+
+test('type error', () => {
+    expect(() => average("not an array")).toThrow(TypeError);
+});
