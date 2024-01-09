0fda79b228b285c20c5240641dd9845c75041b2e
Fanny Gautier <fannygautier92@gmail.com>
2024-01-09 11:41:48 +0100

    fix

--- src/songCreator.js
+++ src/songCreator.js
@@ -159,7 +159,7 @@ export default class SongCreator {
             const k = typeof amount === 'number' ? amount : 50;
             const n_samples = 44100;
             const curve = new Float32Array(n_samples);
-            const deg = Math.PI / 180;
+           
 
             for (let i = 0; i < n_samples; ++i ) {
                 const x = i * 2 / n_samples - 1;
@@ -229,7 +229,7 @@ export default class SongCreator {
      */
     stopTrack(track) {
         track.sequences.forEach(sequence => {
-            sequence.activeNodes.forEach(({ oscillator, gainNode }) => {
+            sequence.activeNodes.forEach(({ oscillator }) => {
                 try {
                     oscillator.stop();
                 } catch (error) {
